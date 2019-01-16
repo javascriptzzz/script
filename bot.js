b42a5ff44295cd72d5d416a7f221406374329658
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-01-15 17:46:27 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,13 +7,13 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.channel.send('PONG!');
+    	message.channel.send('Pong!');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
+    if (message.content === '-noob') {
+    	message.reply('noob!');
   	}
 });
 
@@ -21,6 +21,12 @@ client.on('message', message => {
     if (message.content === 'test') {
         message.reply('test!');
     }
+});
+
+client.on('message', message => {
+    if (message.content === 'noobperson') {
+    	message.reply('_ _');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
