7fc2c1faefd790c0ec30a7f1d6abb872b5c0c824
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-01-16 08:18:48 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,16 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === '-avatar') {
-    msg.reply(message.author.displayAvatarURL());
+client.on('message', message => {
+  // If the message is "what is my avatar"
+  if (message.content === 'what is my avatar') {
+    // Send the user's avatar URL
+    message.reply(message.author.displayAvatarURL());
   }
 });
 
