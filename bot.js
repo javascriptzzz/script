52f1b16df8b6e9187b46c8a5eaf831f6700be1b5
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-01-15 21:55:32 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    console.log('${client.user.name}');
+    console.log('${client.username}');
 });
 
 client.on('message', message => {
