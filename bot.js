44a6b2ed27737fd3602feb0267d67b78a20fafad
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-01-15 18:23:30 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,49 +1,21 @@
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-const randomColor = "#000000".replace(/0/g, function () { return (~~(Math.random() * 16)).toString(16); });
-exports.run = (client, msg, args) => {
-  const member = msg.mentions.members.first() || msg.guild.members.get(args[0]) || msg.member;
-  if (!member) return msg.reply("Please provide a vaild Mention or USER ID");
-  let bot;
-  if (member.user.bot === true) {
-    bot = "Yes";
-  } else {
-    bot = "No";
-  }
-  const embed = new Discord.MessageEmbed()
-    .setColor(randomColor)
-    .setThumbnail(`${member.user.displayAvatarURL()}`)
-    .setAuthor(`${member.user.tag} (${member.id})`, `${member.user.avatarURL()}`)
-    .addField("Nickname:", `${member.nickname !== null ? `Nickname: ${member.nickname}` : "No nickname"}`, true)
-    .addField("Bot?", `${bot}`, true)
-    .addField("Guild", `${bot}`, true)
-    .addField("Status", `${status[member.user.presence.status]}`, true)
-    .addField("Playing", `${member.user.presence.game ? `${member.user.presence.game.name}` : "not playing anything."}`, true)
-    .addField("Roles", `${member.roles.filter(r => r.id !== msg.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "No Roles"}`, true)
-    .addField("Joined At", `${moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true)
-    .addField("Created At", `${moment.utc(member.user.createdAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true);
+client.on('message', message => {
+    if (message.content === '-ping') {
+    	message.reply('Pong!');
+  	}
+});
 
-  msg.channel.send({
-    embed
-  });
-};
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: ["uinfo"],
-  permLevel: 0
-};
-
-exports.help = {
-  name: "userinfo",
-  description: "Gets userinfo from a mention or id",
-  usage: "userinfo <mention> or <id>"
-};
+client.on('message', message => {
+    if (message.content === '-bing') {
+    	message.reply('Bong!');
+  	}
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
