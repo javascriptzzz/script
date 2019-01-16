218f4b5912e055fb4d3cb3c63ad082d561a60342
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-01-16 15:30:00 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = "d";
 
 client.on('ready', () => {
   console.log('I am ready!');
@@ -22,5 +23,27 @@ client.on('message', message => {
     message.channel.send(message.author.displayAvatarURL);
   }
 });
+
+if (`${prefix}help` === 'command') {
+    let embed = new Discord.RichEmbed()
+      .addField('!help', 'gives you this current information')
+      .setTitle('Help')
+      .setColor('#3498db')
+      .addField(
+        '!userinfo',
+        'gives you info about a user(currently being worked on)'
+      )
+      .addField(
+        '!serverinfo',
+        'gives you info about a server(currently working on it)'
+      )
+      .addField('link to support server', 'https://discord.gg/NZ2Zvjm')
+      .addField(
+        'invite link for bot',
+        'https://discordapp.com/api/oauth2/authorize?client_id=449983742224760853&permissions=84993&scope=bot'
+      )
+
+    message.reply("here's a list of commands that i'm able to do")
+    message.channel.send({ embed })
 
 client.login(process.env.BOT_TOKEN);
