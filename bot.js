6020722bc7747b44553889baa49dcf9b4f7f24e8
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-01-16 08:43:30 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,20 @@
-/**
- * Send a user a link to their avatar
- */
-
-// Import the discord.js module
 const Discord = require('discord.js');
-
-// Create an instance of a Discord client
 const client = new Discord.Client();
 
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
-// Create an event listener for messages
 client.on('message', message => {
-  // If the message is "what is my avatar"
-  if (message.content === 'what is my avatar') {
-    // Send the user's avatar URL
-    message.reply(message.author.displayAvatarURL());
-  }
+    if (message.content === 'ping') {
+    	message.channel.send('Pong!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '-ping') {
+    	message.reply('Pong!');
+  	}
 });
 
 client.login(process.env.BOT_TOKEN);
