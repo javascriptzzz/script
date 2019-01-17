8f14d38a8dd215d206618a22e60c0f8d360c6612
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-01-16 22:43:19 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,27 @@
-// Load up the discord.js library
-const Discord = require("discord.js");
 
-// This is your client. Some people call it `bot`, some people call it `self`, 
-// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
-// this is what we're refering to. Your client.
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
-// Here we load the config.json file that contains our token and our prefix values. 
-const prefix = "-";
+client.on('ready', () => {
+    console.log('I am ready!');
+});
 
-//Checks if it starts with (/profile) 
-if(message.content.startsWith(`/profile`)){
-    //Defines the user that needs to be searched to be either the user pinged OR the user that called the command
-    let user = message.mentions.users.first(); || message.author;
-    let embed = new Discord.RichEmbed()
-    .setTitle(`Profile`);
-    .setDescription(`Profile of ${user.username}`);
-    //Note: .send(embed) is allowed but not recommended and it's easier and safer todo .send({embed}) or in your case .send({embed:botembed})
-    message.channel.send({embed});    
-}
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.channel.send('PONG!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'bing') {
+    	message.reply('BONG!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'hmm') {
+        message.react('👙');
+    }
+});
 
 client.login(process.env.BOT_TOKEN);
