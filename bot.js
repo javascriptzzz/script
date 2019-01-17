9f2d76944d867043fa62199dcd383b11752cb582
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-01-16 20:47:17 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,8 +3,7 @@ const client = new Discord.Client();
 const prefix = "d";
 
 client.on("ready", () => {
-    console.log('Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.');
-    client.user.setActivity('F.D.O.J.R.P in ${client.guilds.size} servers');
+    console.log('Bot has started');
 });
 
 client.on('message', message => {
