2d6a3f651b414f3141645de2bdddad0e8bc36007
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-01-16 20:20:39 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,21 +27,4 @@ client.on('message', message => {
   }
 });
 
-client.on("message"), async message => {
- 
-    if (message.author.bot) return;
- 
-    if (message.content.indexOf(config.prefix) !== 0) return;
- 
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
- 
- 
-    if (command === "ping") {
- 
-        const m = await message.channel.send("Ping?");
-        m.edit('Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms');
-    }
-});
-
 client.login(process.env.BOT_TOKEN);
