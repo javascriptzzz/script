3d1917f213a544b61ff443f9003299456e05d36a
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-01-19 01:02:43 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,10 +12,11 @@ bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
-  let prefix = "-";
   let messageArray = message.content.split(" ");
   let command = messageArray[0];
   let args = messageArray.slice(1);
+  
+  if(!command.startswith(prefix)) return;
 
   if(!command === `${prefix}serverinfo`){
 
@@ -30,9 +31,26 @@ bot.on("message", async message => {
     .addField("Total Members", message.guild.memberCount);
 
     return message.channel.send(serverembed);
-  
   }
-  
+});
+
+client.on('message', message => {
+  // If the message is "how to embed"
+  if (message.content === 'how to embed') {
+    // We can create embeds using the MessageEmbed constructor
+    // Read more about all that you can do with the constructor
+    // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
+    const embed = new MessageEmbed()
+      // Set the title of the field
+      .setTitle('A slick little embed')
+      // Set the color of the embed
+      .setColor(0xFF0000)
+      // Set the main content of the embed
+      .setDescription('Hello, this is a slick embed!');
+    // Send the embed to the same channel as the message
+      .addfield("help");
+    message.channel.send(embed);
+  }
 });
 
 bot.login(process.env.BOT_TOKEN);
