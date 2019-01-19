3ffb997fd3d2fb1b38ca6c5ad46a05568f33382a
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-01-18 18:14:01 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const { Client, RichEmbed } = require('discord.js');
 const client = new Discord.Client();
 const prefix = "d";
 
@@ -28,6 +28,24 @@ client.on('message', message => {
     if (message.content === '-avatar') {
         message.reply(message.author.displayAvatarURL);
     }
+});
+
+client.on('message', message => {
+  // If the message is "how to embed"
+  if (message.content === 'how to embed') {
+    // We can create embeds using the MessageEmbed constructor
+    // Read more about all that you can do with the constructor
+    // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
+    const embed = new RichEmbed()
+      // Set the title of the field
+      .setTitle('A slick little embed')
+      // Set the color of the embed
+      .setColor(0xFF0000)
+      // Set the main content of the embed
+      .setDescription('Hello, this is a slick embed!');
+    // Send the embed to the same channel as the message
+    message.channel.send(embed);
+  }
 });
  
 client.login(process.env.BOT_TOKEN);
