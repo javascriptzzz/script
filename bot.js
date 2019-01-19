ec554ea6a85014bc37a3e64606cbe9671f26c81a
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-01-18 21:31:17 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,33 +5,48 @@ const prefix = botSettings.prefix;
 const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
-  console.log("Bot is ready! ${bot.user.username}");
-  
-  try {
-     Let link = await bot.generateInvite(["ADMINISTRATOR"]);
-     console.log(link);
-  } catch(e) {
-     console.log(e.stack);
-  } 
+  console.log(`${bot.user.username} is online!`);
+
+  bot.user.setActivity("tutorials on TSC", {type: "WATCHING"});
+
+  //bot.user.setGame("on SourceCade!");
 });
 
 bot.on("message", async message => {
   if(message.author.bot) return;
-  if(message.channel.type === "dm") return
-  
-  Let messageArray = message.content.split(" ");
-  Let command = messageArray[0];
-  Let args = messageArray.slice(1);
-  
-  if(command.startswith(prefix)) return;
-  
-  if(command === `${prefix}userinfo`) {
-  Let embed = new Discord.RichEmbed()
-     .setAuthor(message.author.username);
-     .setDescription("This is the user's info");
-     message.channel.sendEmbed(embed);
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if(cmd === `${prefix}kick`){
+
+    //!kick @daeshan askin for it
+
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!kUser) return message.channel.send("Can't find user!");
+    let kReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+
+    let kickEmbed = new Discord.RichEmbed()
+    .setDescription("~Kick~")
+    .setColor("#e56b00")
+    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+    .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+    .addField("Kicked In", message.channel)
+    .addField("Tiime", message.createdAt)
+    .addField("Reason", kReason);
+
+    let kickChannel = message.guild.channels.find(`name`, "incidents");
+    if(!kickChannel) return message.channel.send("Can't find incidents channel.");
+
+    message.guild.member(kUser).kick(kReason);
+    kickChannel.send(kickEmbed);
+
+    return;
   }
-   
-});
 
 bot.login(process.env.BOT_TOKEN);
