62a2aaea6fdb72edceebea13c42a9f5c09d6f4db
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-01-18 18:33:31 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ client.on('message', message => {
       // Set the color of the embed
       .setColor(0xFF0000)
       .addfield("name")
-      .addfield("test")
+      .addfield("test");
       // Set the main content of the embed
       .setDescription('Hello, this is a slick embed!');
     // Send the embed to the same channel as the message
