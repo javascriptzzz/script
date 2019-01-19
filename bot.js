ba4e6e5b6447bc07a9606989a61d039e5700da72
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-01-18 18:18:31 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,14 @@
-const { Discord, Client, RichEmbed } = require('discord.js');
-const client = new Discord.Client();
-const prefix = "d";
+const { Client, RichEmbed } = require('discord.js');
 
+// Create an instance of a Discord client
+const client = new Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
 client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-        message.channel.send('PONG!');
-      }
-});
-
-client.on('message', message => {
-    if (message.content === 'bing') {
-        message.reply('BONG!');
-      }
-});
-
-client.on('message', message => {
-    if (message.content === 'hmm') {
-        message.react('🤔');
-    }
-});
-
-client.on('message', message => {
-    if (message.content === '-avatar') {
-        message.reply(message.author.displayAvatarURL);
-    }
+  console.log('I am ready!');
 });
 
 client.on('message', message => {
