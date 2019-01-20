ec877ea81315a37c39ff3ba17b94ebf3a3cd7c1b
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-01-19 16:44:42 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,34 +19,8 @@ bot.on("message", async message => {
   
   if(!command.startsWith(prefix)) return;
 
-  if(command === `${prefix}serverinfo`) {
-
-     let sicon = message.guild.iconURL;
-     let serverembed = new Discord.RichEmbed()
-     .setDescription("Server Information")
-     .setColor("#15f153")
-     .setThumbnail(sicon)
-     .addField("Server Name", message.guild.name)
-     .addField("Created On", message.guild.createdAt)
-     .addField("You Joined", message.member.joinedAt)
-     .addField("Total Members", message.guild.memberCount);
-
-     message.channel.send(serverembed);
-   }
-});
-
-bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
-
-  let messageArray = message.content.split(" ");
-  let command = messageArray[0];
-  let args = messageArray.slice(1);
-  
-  if(!command.startsWith(prefix)) return;
-
   if(command === `${prefix}userinfo`) {
-     console.log("user have used userinfo command");
+     message.channel.send("test");
   }
 });
 
