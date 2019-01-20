07099c222a1e2791d7e422c0710562f31ba6bb0c
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-01-19 21:28:21 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,9 +41,7 @@ bot.on("message", async message => {
         
         let toMute = message.mentions.users.first() || message.guild.members.get(args[0]);
         if(!toMute) return message.channel.sendMessage("You did not specify a user mention or ID");
-      
-        return;
-      }
+    }
 });
 
 bot.login(process.env.BOT_TOKEN);
