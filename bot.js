522e2f0a3cb9f1d4ba6afb4e19d47366a76d7d77
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-03-25 15:07:06 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,36 @@
-const 
+const botsettings = require("./botsettings.json");
+const Discord = require("discord.js");
+const prefix = botsettings.prefix;
+
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  
+});
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+  
+  let messageArray = message.content.split(" ");
+  let command = messageArray[0];
+  let args = messageArray.slice(1);
+  
+  if(!command.startsWith(prefix)) return;
+  
+  if(command === `${prefix}userinfo`) {
+    let embed = new Discord.RichEmbed()
+        .setAuthor(message.author.username)
+        .setDescription("This is the user's info")
+        .setColor("#FFFF")
+        .addField("Full Username:", `${message.author.username}#${message.author.discriminator}`)
+        .addField("User ID:", message.author.id)
+        .addField("Created at", message.author.createdAt)
+    
+    message.channel.sendEmbed(embed);
+  
+  }
+});
+
+bot.login(process.env.BOT_TOKEN);
