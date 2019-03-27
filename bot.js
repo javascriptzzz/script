0e99d6184086838e7555c499db1481e120bfc1e3
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-03-26 21:06:33 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  
+  bot.user.setActivity(`testing`);  
 });
 
 bot.on("message", async message => {
