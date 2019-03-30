9bfaeec5d9da11f4d2bae17bd906a29a2cde30be
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-03-29 22:49:15 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -113,10 +113,23 @@ bot.on("message", async message => {
       return message.channel.send("You did not specify a user mention or ID");
     let role = message.guild.roles.find(r => r.name === "Muted");
     if(toMute.roles.has(role.id))
-      return message.channel.send("This user is already muted");
+      return message.channel.send("This user is already muted!");
     await toMute.addRole(role);
     message.channel.send("I have muted them");
   }
+  
+  if(command === `${prefix}unmute`) {
+  if(!message.member.hasPermission("MANAGE_MESSAGES"))
+    return message.channel.send("You dont have manage message")
+    
+  let toMute = message.mentions.members.first() || message.guild.members.get(args[0]);
+  if(!toMute)
+    return message.channel.send("You did not specify a user mention or ID");
+  let role = message.guild.roles.find(r => r.name === "Muted");
+  if(!role || !toMute.roles.has(role.id))
+    return message.channel.send("This user is not muted!");
+  await toMute.removeRole(role);
+  message.channel.send("I have unmuted them");
 });
 
 bot.login(process.env.BOT_TOKEN);
